53f5ea63217c74e13f3c0696d48d222ffa75c3b3
Fran <Fran@DESKTOP-3JOP471>
2024-10-06 18:40:22 +0200

    1-5 DONE

--- Tema2/src/boletin1/Ejercicio5.java
+++ Tema2/src/boletin1/Ejercicio5.java
@@ -15,6 +15,15 @@ public class Ejercicio5 {
 		// Creamos la variable para el tercer número
 		double num3;
 
+		// Creamos una variable para guardar el número mayor
+		double mayor = 0;
+
+		// Creamos una variable para guardar el númeor medio
+		double medio = 0;
+
+		// Creamos una variable para guardar el número menor
+		double menor = 0;
+
 		// Abrimos scanner
 		Scanner rd = new Scanner(System.in);
 
@@ -31,18 +40,39 @@ public class Ejercicio5 {
 		num3 = rd.nextDouble();
 
 		// Abrimos condicional para ordenar los números
-		if ((num1 > num2 && num1 > num3)
-				&& ((num2 < num1 && num2 > num3) || (num2 < num3 && num2 > num1) && (num3 < num1 && num3 < num2))) {
-			// Si es el menor se pone el primer dígito
-			System.out.println(num1 + " > " + num2 + " > " + num3);
-		} else if ((num2 > num1 && num2 > num3)
-				&& ((num1 < num2 && num1 > num3) || (num1 < num3 && num1 > num2) && (num3 < num2 && num3 < num1))) {
-			// Si es el menor se pone el primer dígito
-			System.out.println(num2 + " > " + num1 + " > " + num3);
-		}
-			else {
-				
+		if (num1 >= num2 && num1 >= num3) {
+			mayor = num1;
+			if (num2 >= num3) {
+				medio = num2;
+				menor = num3;
+			} else {
+				medio = num3;
+				menor = num2;
 			}
-			
 		}
+		if (num2 >= num1 && num2 >= num3) {
+			mayor = num2;
+			if (num1 >= num3) {
+				medio = num1;
+				menor = num3;
+			} else {
+				medio = num3;
+				menor = num1;
+			}
+		}
+		if (num3 >= num1 && num3 >= num2) {
+			mayor = num3;
+			if (num1 >= num2) {
+				medio = num1;
+				menor = num2;
+			} else {
+				medio = num2;
+				menor = num1;
+			}
+		}
+		System.out.println("Números ordenados: " + mayor + " > " + medio + " > " + menor);
+		
+		//Cerramos scanner
+		rd.close();
 	}
+}
