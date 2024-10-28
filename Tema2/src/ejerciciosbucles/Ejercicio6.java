84affb8e9ed516ce0bc1accc7aa4f0487262be36
Fran <Fran@DESKTOP-3JOP471>
2024-10-28 13:17:30 +0100

    6 not DONE

--- Tema2/src/ejerciciosbucles/Ejercicio6.java
+++ Tema2/src/ejerciciosbucles/Ejercicio6.java
@@ -1,5 +1,16 @@
 package ejerciciosbucles;
 
 public class Ejercicio6 {
-
+	public static void main(String[] args) {
+		/*
+		 * Solicita al usuario un número n y dibuja un triángulo de base y altura n. Por
+		 * ejemplo para n=4 debe dibujar lo siguiente:
+		 *        *
+		 *       * *
+		 *      * * *
+		 *     * * * *
+		 */
+		
+		
+	}
 }
